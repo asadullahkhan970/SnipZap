fea0fbe52016ca2e37b30293d9b15de0eec83a87
Asadullah-khan596 <asadullahkhan3096@gmail.com>
2024-10-01 12:56:50 +0530

    Updated

--- frontend/src/component/LoginCard.jsx
+++ frontend/src/component/LoginCard.jsx
@@ -17,7 +17,7 @@ import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { useSetRecoilState } from "recoil";
 import authScreenAtom from "../atoms/authAtom";
-import useShowToast from "../hooks/useShowToast";
+import useShowToast from "../hooks/useshowToast";
 import userAtom from "../atoms/userAtom";
 
 export default function LoginCard() {
