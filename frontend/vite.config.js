fad86c9246081ccd68d42275ad8c489513bc4cec
Asadullah-khan596 <asadullahkhan3096@gmail.com>
2024-10-01 12:41:18 +0530

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    //Get rid of CORS error
-    proxy:{
-        "/api":{
-          target:"http://localhost:5000",
-          ckangeOrigin:true,
-          secure:false,
-        }
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: process.env.API_URL || 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
-})
+});
