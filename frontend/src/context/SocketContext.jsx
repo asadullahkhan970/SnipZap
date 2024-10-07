7744570aa4d20a310cb1bc1104585a7e2335144b
Asadullah-khan596 <asadullahkhan3096@gmail.com>
2024-10-07 17:48:26 +0530

    updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,42 +1,33 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
-import userAtom from "../atoms/userAtom";
 import io from "socket.io-client";
+import userAtom from "../atoms/userAtom";
 
 const SocketContext = createContext();
 
-export const useSocket = () =>{
-    return useContext(SocketContext);
-}
+export const useSocket = () => {
+	return useContext(SocketContext);
+};
 
-export const SocketContextProvider = ({children}) =>{
-    const [socket,setSocket] = useState(null);
-    const [onlineUsers,setOnlineUsers] = useState([]);
-    const user = useRecoilValue(userAtom);
+export const SocketContextProvider = ({ children }) => {
+	const [socket, setSocket] = useState(null);
+	const [onlineUsers, setOnlineUsers] = useState([]);
+	const user = useRecoilValue(userAtom);
 
-    useEffect(() => {
-        if (user?._id) {
-          const socket = io("http://localhost:5000", {
-            query: {
-              userId: user._id, // Pass user ID from the authenticated user
-            },
-          });
-      
-          setSocket(socket);
-      
-          // Get online users from the server
-          socket.on("getOnlineUsers", (users) => {
-            setOnlineUsers(users); // Update state with the list of online users
-            console.log("Online users:", users); // Debug log
-          });
-      
-          return () => {
-            socket.disconnect(); // Clean up when component unmounts
-          };
-        }
-      }, [user?._id]);
-      
-    console.log("online users",onlineUsers)
-    return <SocketContext.Provider value={{socket,onlineUsers}}>{children}</SocketContext.Provider>;
+	useEffect(() => {
+		const socket = io("/", {
+			query: {
+				userId: user?._id,
+			},
+		});
 
+		setSocket(socket);
+
+		socket.on("getOnlineUsers", (users) => {
+			setOnlineUsers(users);
+		});
+		return () => socket && socket.close();
+	}, [user?._id]);
+
+	return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
 };
